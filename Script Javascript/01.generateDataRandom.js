aaf00bcb99137eceb389aee4bff78a6ef6449090
ytbhoanghai <56905615+ytbhoanghai@users.noreply.github.com>
2020-03-12 18:47:03 +0700

    Update 01.generateDataRandom.js

--- Script Javascript/01.generateDataRandom.js
+++ Script Javascript/01.generateDataRandom.js
@@ -1,13 +1,14 @@
 function generateDataRandom(obj) {
+	
 	let phoneNumber1 = `0${Math.floor(Math.random() * 1000000000)}`;
-    let phoneNumber2 = NumberInt(phoneNumber1);
-    let customRating = Number.parseFloat((Math.random() * 10).toFixed(1));
+	let phoneNumber2 = NumberInt(phoneNumber1);
+	let customRating = Number.parseFloat((Math.random() * 10).toFixed(1));
     
-    obj.phoneNumber1 = phoneNumber1;
-    obj.phoneNumber2 = phoneNumber2;
-    obj.customRating = customRating;
+	obj.phoneNumber1 = phoneNumber1;
+	obj.phoneNumber2 = phoneNumber2;
+	obj.customRating = customRating;
 
-    return obj;
+	return obj;
 }
 
 db.restaurants.find().forEach((object) => {
