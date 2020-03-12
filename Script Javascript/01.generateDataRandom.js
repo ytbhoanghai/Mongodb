4fe05f8eb776039ccde1f149da156fe91221d6b5
yytbhoanghai <devilistears@yahoo.com.vn>
2020-03-12 17:08:28 +0700

    update file 01.generateDataRandom.js

---
+++ Script Javascript/01.generateDataRandom.js
@@ -0,0 +1,15 @@
+function generateDataRandom(obj) {
+	let phoneNumber1 = `0${Math.floor(Math.random() * 1000000000)}`;
+    let phoneNumber2 = NumberInt(phoneNumber1);
+    let customRating = Number.parseFloat((Math.random() * 10).toFixed(1));
+    
+    obj.phoneNumber1 = phoneNumber1;
+    obj.phoneNumber2 = phoneNumber2;
+    obj.customRating = customRating;
+
+    return obj;
+}
+
+db.restaurants.find().forEach((object) => {
+	db.restaurants.save(generateDataRandom(object));
+});
